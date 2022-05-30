92c225107e68ceac06209a4a2b3f7fbc1220a10b
dhkstnaos <dhkstnaos@gmail.com>
2022-05-31 04:08:34 +0900

    :sparkles: 무한 스트림 iterate, generate

--- src/fifth/main.java
+++ src/fifth/main.java
@@ -2,8 +2,14 @@ package fifth;
 
 import fourth.Dish;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.IntSupplier;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static fourth.Dish.menu;
@@ -23,7 +29,7 @@ public class main {
             new Transaction(alan, 2012, 950)
     );
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //1. 2011년에 일어난 모든 트랜잭션을 찾아 값을 오름차순으로 정리하라.
         List<Transaction> first = transactions.stream()
                 .filter(transaction -> transaction.getYear() == 2011)
@@ -83,5 +89,67 @@ public class main {
 
         String home = System.getProperty("home");
         Stream<String> stream= home == null ? Stream.empty() : Stream.of(home);
+
+        Stream<String> stringStream = Stream.ofNullable(System.getProperty("home"));
+
+        int[] numsArray={1,2,3,4,5};
+        int sum1 = Arrays.stream(numsArray).sum();
+
+        // Stream.iterate
+        Stream.iterate(0, n -> n + 2)
+                .limit(10)
+                .forEach(System.out::println);
+
+        // iterate를 이용한 피보나치
+        Stream.iterate(new int[] { 0, 1 }, t -> new int[] { t[1], t[0] + t[1] })
+                .limit(10)
+                .forEach(t -> System.out.printf("(%d, %d)", t[0], t[1]));
+
+        Stream.iterate(new int[] { 0, 1 }, t -> new int[] { t[1], t[0] + t[1] })
+                .limit(10)
+                .map(t -> t[0])
+                .forEach(System.out::println);
+
+        // Stream.generate를 이용한 임의의 double 스트림
+        Stream.generate(Math::random)
+                .limit(10)
+                .forEach(System.out::println);
+
+        // Stream.generate을 이용한 요소 1을 갖는 스트림
+        IntStream.generate(() -> 1)
+                .limit(5)
+                .forEach(System.out::println);
+
+        IntStream.generate(new IntSupplier() {
+            @Override
+            public int getAsInt() {
+                return 2;
+            }
+        }).limit(5).forEach(System.out::println);
+
+        IntSupplier fib = new IntSupplier() {
+
+            private int previous = 0;
+            private int current = 1;
+
+            @Override
+            public int getAsInt() {
+                int nextValue = previous + current;
+                previous = current;
+                current = nextValue;
+                return previous;
+            }
+
+        };
+        IntStream.generate(fib)
+                .limit(10)
+                .forEach(System.out::println);
+
+        long uniqueWords = Files.lines(Paths.get("lambdasinaction/chap5/data.txt"), Charset.defaultCharset())
+                .flatMap(line -> Arrays.stream(line.split(" ")))
+                .distinct()
+                .count();
+
+        System.out.println("There are " + uniqueWords + " unique words in data.txt");
     }
 }
