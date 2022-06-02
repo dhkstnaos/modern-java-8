7802a21f2ece7c1a3cbd81d6431d4df4a02b086d
dhkstnaos <dhkstnaos@gmail.com>
2022-06-02 16:18:03 +0900

    :sparkles: Grouping 개선

--- src/sixth/Grouping.java
+++ src/sixth/Grouping.java
@@ -1,10 +1,11 @@
 package sixth;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.*;
+import static sixth.Dish.dishTags;
 import static sixth.Dish.menu;
 import static sixth.Grouping.CaloricLevel.chooseCaloricLevel;
 
@@ -16,6 +17,11 @@ public class Grouping {
         System.out.println("groupDishNamesByType() = " + groupDishNamesByType());
         System.out.println("mostCaloricDishesByType() = " + mostCaloricDishesByType());
         System.out.println("sumCaloriesByType() = " + sumCaloriesByType());
+        System.out.println("groupDishTagsByType() = " + groupDishTagsByType());
+        System.out.println("groupDishedByTypeAndCaloricLevel() = " + groupDishedByTypeAndCaloricLevel());
+        System.out.println("subGroupCountingByType() = " + subGroupCountingByType());
+        System.out.println("mostCaloricDishesByTypeWithoutOptional() = " + mostCaloricDishesByTypeWithoutOptional());
+        System.out.println("caloricLevelsByType() = " + caloricLevelsByType());
     }
 
     private static Map<Dish.Type, List<Dish>> dishesByType() {
@@ -68,6 +74,52 @@ public class Grouping {
                 groupingBy(Dish::getType, summingInt(Dish::getCalories)));
     }
 
+    private static Map<Dish.Type, Set<String>> groupDishTagsByType() {
+        return menu.stream().collect(
+                groupingBy(Dish::getType,
+                        flatMapping(dish -> dishTags.get(dish.getName()).stream(), toSet())));
+    }
+
+    private static Map<Dish.Type, Map<CaloricLevel, List<Dish>>> groupDishedByTypeAndCaloricLevel() {
+        return menu.stream().collect(
+                groupingBy(Dish::getType,
+                        groupingBy(dish -> chooseCaloricLevel(dish.getCalories()))
+                )
+        );
+    }
+
+    private static Map<Dish.Type, Long> subGroupCountingByType() {
+        return menu.stream().collect(
+                groupingBy(Dish::getType,
+                        Collectors.counting()
+                )
+        );
+    }
+
+    private static Map<Dish.Type, Dish> mostCaloricDishesByTypeWithoutOptional() {
+//        return menu.stream().collect(
+//                groupingBy(Dish::getType,
+//                        collectingAndThen(
+//                                reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2),
+//                                Optional::get)));
+        //reducing보다는 Collectors의 메서드를 활용하자
+        return menu.stream().collect(
+                groupingBy(Dish::getType,
+                        collectingAndThen(
+                                Collectors.maxBy(comparingInt(Dish::getCalories)),
+                                Optional::get)
+                )
+        );
+    }
+
+    private static Map<Dish.Type, Set<CaloricLevel>> caloricLevelsByType() {
+        return menu.stream().collect(
+                groupingBy(Dish::getType,
+                        mapping(
+                                dish -> chooseCaloricLevel(dish.getCalories()), toCollection(HashSet::new)
+                        ))
+        );
+    }
 
     public enum CaloricLevel {
         DIET, NORMAL, FAT;
