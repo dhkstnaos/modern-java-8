5b1127b8da781073ccfb69cab314dddb557b1201
dhkstnaos <dhkstnaos@gmail.com>
2022-06-03 01:19:24 +0900

    :sparkles: removeIf(Predicate) 활용한 요소 제거

--- src/eight/CollectionFactory.java
+++ src/eight/CollectionFactory.java
@@ -1,5 +1,8 @@
 package eight;
 
+import fifth.Trader;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,5 +16,18 @@ public class CollectionFactory {
         Set<String> sets = Set.of("dhkstn", "frank", "josh");
 
         Map<String, Integer> maps = Map.of("dhkstn", 10, "frank", 20, "josh", 30);
+
+        //setting
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(new Transaction(new Trader("do", "00213"), 2022, 3000));
+        transactions.add(new Transaction(new Trader("do", "dsf"), 2012, 2000));
+        transactions.add(new Transaction(new Trader("vv", "453"), 2002, 5000));
+        transactions.add(new Transaction(new Trader("vv", "df"), 2012, 8000));
+        transactions.add(new Transaction(new Trader("do", "yongin"), 2032, 2000));
+        transactions.add(new Transaction(new Trader("vv", "yongin"), 2002, 1000));
+
+        System.out.println(transactions);
+        transactions.removeIf(transaction -> Character.isDigit(transaction.getTrader().getCity().charAt(0)));
+        System.out.println(transactions);
     }
 }
